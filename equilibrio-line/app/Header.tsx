3f0986dd001f07ef6543f071e86c7426c4eb0756
vinicio.barbosa <vinicio.barbosa@gx2.com.br>
2025-04-05 01:08:39 -0300

    First global configuration

---
+++ equilibrio-line/app/Header.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import logo from "../public/Equilibrio-line-logo.png"
+export default function Header() {
+  return (
+    <header className="h-[84px] w-full bg-white flex items-center text-black font-[adelia]">
+      <div className='flex items-center justify-center'>
+        <Image src={logo}
+          width={80}
+          height={80}
+          alt="Logo Equilibrio Line" />
+        <h1>Equlibrio Line</h1>
+      </div>
+    </header>
+  );
+}
---
+++ equilibrio-line/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local';
+import Header from "./Header"
+import "./globals.css";
+
+const adelia = localFont({
+  src: [
+    {
+      path: './fonts/adelia.woff',
+    }
+  ],
+  variable: '--font-adelia'
+})
+export const metadata: Metadata = {
+  title: "EquilibrioLine",
+  description: "Clínica española de embellecimiento",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body
+        className={`${adelia.variable} antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
