233b4943973c7a02ad70abe48f5374c3b146e906
vinicio.barbosa <vinicio.barbosa@gx2.com.br>
2025-04-05 15:34:37 -0300

    Change global css and make a template for header

--- equilibrio-line/app/Header.tsx
+++ equilibrio-line/app/Header.tsx
@@ -2,14 +2,25 @@ import Image from 'next/image'
 import logo from "../public/Equilibrio-line-logo.png"
 export default function Header() {
   return (
-    <header className="h-[84px] w-full bg-white flex items-center text-black font-[adelia]">
-      <div className='flex items-center justify-center'>
+    <header className="h-[84px] w-full bg-white flex items-cente px-5 justify-between">
+      <div className='flex items-center justify-center  font-[adelia]'>
         <Image src={logo}
           width={80}
           height={80}
           alt="Logo Equilibrio Line" />
         <h1>Equlibrio Line</h1>
       </div>
+      <nav className="flex items-center justify-end space-x-3">
+        <a href="#" className="text-sm font-medium hover:text-gray-900">
+          Inicio
+        </a>
+        <a href="#" className="text-sm font-medium hover:text-gray-900">
+          Servicios
+        </a>
+        <a href="#" className="text-sm font-medium hover:text-gray-900">
+          Contacto
+        </a>
+      </nav>
     </header>
   );
 }
