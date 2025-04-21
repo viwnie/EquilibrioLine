8bf3af4faf5e820dc3628f851e19f483cf8b2916
vinicio.barbosa <vinicio.barbosa@gx2.com.br>
2025-04-21 12:13:20 -0300

    finishheader skeleton

--- equilibrio-line/app/layout.tsx
+++ equilibrio-line/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from 'next/font/local';
 import Header from "./components/Header"
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const adelia = localFont({
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
