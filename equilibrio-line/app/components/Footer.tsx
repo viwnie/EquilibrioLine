7a39dc11f5b4fb1cfc3a22cb5ddbf31fe27596ef
Renan <renan.casaeses@gmail.com>
2025-04-22 19:47:59 -0300

    footer new branch

--- equilibrio-line/app/components/Footer.tsx
+++ equilibrio-line/app/components/Footer.tsx
@@ -1,9 +1,23 @@
-
-
 const Footer = () => {
-  return (
-    <h1>teste</h1>
-  )
-}
+	return (
+		<div className="bg-[#221E1F] px-4 py-[30px] text-white">
+			<div className="flex justify-evenly mt-3">
+				<h1 className="font-[adelia]">Equlibrio Line</h1>
+				<span>Direccion: C/Estación, 27, CP 30500 Molina de Segura (Murcia)</span>
+				<span>Teléfono: +34 613 413 279</span>
+				<div className="flex flex-col gap-2">
+					<span>Lunes a Viernes de 7:00 a 22:00</span>
+					<span>Sábados de 8:00 a 13:00</span>
+				</div>
+			</div>
 
-export default Footer
+      <div>
+        <img src="../assets/Instagram Design Desafio 3.png" alt="" />
+      <h1>kasdklaass</h1>
+      </div>
+      
+		</div>
+	);
+};
+
+export default Footer;
