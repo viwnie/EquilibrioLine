e9bd15f105372c8c2ac701f483fdd7ca2bae1283
nanerseasac <48884311+nanerseasac@users.noreply.github.com>
2025-05-01 01:08:46 -0300

    Revert "Feat/footer2"

--- equilibrio-line/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import  CarouselHome  from './components/CarouselHome';
-
-export default function Home() {
-  return (
-    <div className='flex items-center justify-center'>
-      <CarouselHome />
-    </div>
-  );
-}
