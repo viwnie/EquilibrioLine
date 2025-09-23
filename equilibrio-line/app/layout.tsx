c0324ba2de7fc945d38c62ecf0d0815f320fbc35
vinicio.barbosa <vinicio.barbosa@gx2.com.br>
2025-09-23 18:30:19 -0300

    feat(analytics): add facebook pixel tracking script

--- equilibrio-line/app/layout.tsx
+++ equilibrio-line/app/layout.tsx
@@ -68,6 +68,21 @@ export default function RootLayout({
     <html lang="es" className="h-full">
       <head>
         <StructuredData />
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1477042056937834');
+          fbq('track', 'PageView');
+        ` }} />
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1477042056937834&ev=PageView&noscript=1" />
+        </noscript>
       </head>
       <body
         className={`${adelia.variable} antialiased flex flex-col min-h-screen`}
